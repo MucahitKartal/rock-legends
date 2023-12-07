4312466601120a29f0471c31e9e8679f32fdc515
Mucahit <mucahit.kartal2015@gmail.com>
2023-12-07 05:00:43 +0300

    prevented toggling

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -3,14 +3,24 @@ import useSound from "use-sound";
 
 const Audio = ({ audio }) => {
   const [play, { stop }] = useSound(audio);
+
+  const handlePlay = (e) => {
+    e.stopPropagation(); // Stop the event from propagating to the parent div
+    play();
+  };
+
+  const handleStop = (e) => {
+    e.stopPropagation(); // Stop the event from propagating to the parent div
+    stop();
+  };
   return (
     <div>
-      <button onClick={() => play()}>
+      <button onClick={handlePlay}>
         <span role="img" aria-label="nota">
           🎵 Play
         </span>
       </button>
-      <button onClick={() => stop()}>
+      <button onClick={handleStop}>
         <span role="img" aria-label="stop">
           🛑 Stop
         </span>
